c0d152da23db15baec87d7000a9d772abc3a4e02
Jonathan Miller <jon@jmill.dev>
2022-05-15 20:41:12 -0400

    Fix prompt formatting and add settings manager

--- doomer.py
+++ doomer.py
@@ -1,37 +1,48 @@
+from dataclasses import dataclass
+from datetime import timedelta
 import logging
-from random import random
+from pathlib import Path
+from random import random, randint
 import discord
 import re
 import base64
 from discord.ext import commands
-from marsbots_core import config
 from marsbots_core.models import ChatMessage
 from marsbots_core.programs.lm import complete_text
 from marsbots_core.resources.discord_utils import (
-    filter_application_command_messages,
     get_discord_messages,
     is_mentioned,
     replace_mentions_with_usernames,
+    wait_for_user_reply,
 )
 from marsbots_core.resources.language_models import OpenAIGPT3LanguageModel
+from marsbots_core.resources.settings_manager import LocalSettingsManager
+
+
+@dataclass
+class DoomerSettings:
+    frequency_penalty: float = 1.0
+    presence_penalty: float = 0.2
+    autoreply_probability: float = 0.01
 
 
 class DoomerCog(commands.Cog):
     def __init__(self, bot: commands.bot) -> None:
         self.bot = bot
-        self.language_model = OpenAIGPT3LanguageModel(
-            frequency_penalty=1.0,
-            presence_penalty=0.2,
+        self.settings_path = Path("../doomer_settings.json")
+        self.settings_manager = LocalSettingsManager(
+            self.settings_path,
+            defaults=DoomerSettings(),
         )
-        self.response_thresh = 0.01
-        self.prohibited_words = self.read_prohibited_words(self, "filtered_words.txt")
+        self.prohibited_words = self.read_prohibited_words("filtered_words.txt")
 
     @commands.Cog.listener("on_message")
     async def on_message(self, message: discord.Message) -> None:
+        ctx = await self.bot.get_context(message)
         if (
             is_mentioned(message, self.bot.user)
             or self.bot.settings.name.lower() in message.content.lower()
-            or (not message.author.bot and self.should_act())
+            or (not message.author.bot and self.should_act(ctx))
         ):
             ctx = await self.bot.get_context(message)
             async with ctx.channel.typing():
@@ -70,28 +81,46 @@ class DoomerCog(commands.Cog):
         ),
     ):
         await ctx.defer()
-        completion = await complete_text(
-            self.language_model, prompt, max_tokens=max_tokens
-        )
-        completion_filtered = await self.filter_completion(self, completion)
-        formatted = f"**{prompt}**{completion_filtered}"
+        language_model = self.get_language_model(ctx)
+        completion = await complete_text(language_model, prompt, max_tokens=max_tokens)
+        # completion_filtered = await self.filter_completion(completion)
+        formatted = f"**{prompt}**{completion}"
         await ctx.respond(formatted)
 
-    def should_act(self) -> bool:
+    def should_act(self, ctx) -> bool:
         r = random()
-        return r < self.response_thresh
+        channel_id, guild_id = ctx.channel.id, ctx.guild.id
+        autoreply_probability = self.settings_manager.get_channel_setting(
+            channel_id, guild_id, "autoreply_probability"
+        )
+        return r < autoreply_probability
 
     async def get_completion_with_chat_context(self, ctx, n_messages):
         prompt = await self.format_prompt(ctx, n_messages)
+        language_model = self.get_language_model(ctx)
         completion = await complete_text(
-            self.language_model, prompt, max_tokens=80, stop=["**[", "\n\n"]
+            language_model, prompt, max_tokens=80, stop=["**[", "\n\n"]
         )
-        completion_filtered = await self.filter_completion(self, completion)
-        return completion_filtered
+        return completion
+        # completion_filtered = await self.filter_completion(self, completion)
+        # return completion_filtered
 
     async def filter_completion(self, completion: str) -> str:
-        regex = re.compile('|'.join(map(re.escape, self.prohibited_words)))
+        regex = re.compile("|".join(map(re.escape, self.prohibited_words)))
         return regex.sub("####", completion)
+
+    def get_language_model(self, ctx):
+        channel_id, guild_id = ctx.channel.id, ctx.guild.id
+        frequency_penalty = self.settings_manager.get_channel_setting(
+            channel_id, guild_id, "frequency_penalty"
+        )
+        presence_penalty = self.settings_manager.get_channel_setting(
+            channel_id, guild_id, "presence_penalty"
+        )
+        return OpenAIGPT3LanguageModel(
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+        )
 
     async def format_prompt(self, ctx, n_messages):
         last_messages = await get_discord_messages(ctx.channel, n_messages)
@@ -106,13 +135,18 @@ class DoomerCog(commands.Cog):
                     ChatMessage(
                         self.message_preprocessor(m),
                         m.author.display_name,
+                        deliniator_left=f"**[{m.created_at.strftime('%I:%M:%S %p')} ",
                     )
                 ).strip()
                 for m in last_messages
             ]
         )
+        n_seconds = randint(5, 120)
+        new_msg_time = (
+            last_messages[-1].created_at + timedelta(seconds=n_seconds)
+        ).strftime("%I:%M:%S %p")
         prompt += "\n"
-        prompt += f"**[{self.bot.user.display_name}]**:"
+        prompt += f"**[{new_msg_time} {self.bot.user.display_name}]**:"
         return prompt
 
     def message_preprocessor(self, message: discord.Message) -> str:
@@ -124,11 +158,80 @@ class DoomerCog(commands.Cog):
 
     def read_prohibited_words(self, filename: str) -> list[str]:
         try:
-            return base64.b64decode(open(filename, "r").read()).decode('utf-8').split("\r\n")
+            return (
+                base64.b64decode(open(filename, "r").read())
+                .decode("utf-8")
+                .split("\r\n")
+            )
         except OSError:
             logging.error(f"Unable to open file: {filename}")
-            return ['']
+            return [""]
 
+    ### SETTINGS
+
+    @commands.slash_command()
+    async def update_settings(
+        self,
+        ctx,
+        setting: discord.Option(
+            str,
+            description="Setting name to update",
+            required=True,
+            choices=list(DoomerSettings.__dataclass_fields__.keys()),
+        ),
+        channel_name: discord.Option(
+            str,
+            description="Channel to update setting for",
+            required=False,
+        ),
+    ):
+
+        if channel_name:
+            await self.handle_update_channel_settings(ctx, setting, channel_name)
+        else:
+            await self.handle_update_settings(ctx, setting)
+
+    async def handle_update_settings(self, ctx, setting):
+        await ctx.respond(
+            f"Enter a new value for {setting}. (Currently"
+            f" {self.settings_manager.get_setting(ctx.guild.id, setting)})",
+        )
+        resp = await wait_for_user_reply(self.bot, ctx.author.id)
+        try:
+            new_val = DoomerSettings.__dataclass_fields__[setting].type(
+                resp.content,
+            )
+        except ValueError:
+            await ctx.send(f"{resp.content} is not a valid value for {setting}")
+            return
+        self.settings_manager.update_setting(ctx.guild.id, setting, new_val)
+        await ctx.send(f"Updated {setting} to {new_val}")
+
+    async def handle_update_channel_settings(self, ctx, setting, channel_name):
+        channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+        if not channel:
+            await ctx.respond(f"No channel named {channel_name}")
+            return
+
+        await ctx.respond(
+            f"Enter a new value for {setting}. (Currently"
+            f" {self.settings_manager.get_channel_setting(channel.id, ctx.guild.id, setting)})",
+        )
+        resp = await wait_for_user_reply(self.bot, ctx.author.id)
+        try:
+            new_val = DoomerSettings.__dataclass_fields__[setting].type(
+                resp.content,
+            )
+        except ValueError:
+            await ctx.send(f"{resp.content} is not a valid value for {setting}")
+            return
+        self.settings_manager.update_channel_setting(
+            channel.id,
+            ctx.guild.id,
+            setting,
+            new_val,
+        )
+        await ctx.send(f"Updated {setting} to {new_val}")
 
 
 def setup(bot: commands.Bot) -> None:
--- pm2.config.js
+++ pm2.config.js
@@ -6,8 +6,8 @@ module.exports = {
       interpreter: "python",
       args: "./doomer.json --cog-path=bots.doomer2.doomer --dotenv-path=.env",
       watch: ["."],
-      ignore_watch: ["__pycache__", "*.pyc"],
-      watch_delay: 1000
+      ignore_watch: ["__pycache__", "*.pyc", "./doomer_settings.json"],
+      watch_delay: 1000,
     },
   ],
 };
